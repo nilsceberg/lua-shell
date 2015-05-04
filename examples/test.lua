4dfd3a7231dcd059c771d56b55cad561d46d7faf
Nils Ceberg <nils.ceberg@gmail.com>
2015-05-05 00:44:54 +0200

    Fix example file

--- examples/test.lua
+++ examples/test.lua
@@ -4,7 +4,7 @@ end
 
 function formatter(prefix) print(prefix .. io.read("*a")) end
 
-ls(get_ls_args()):grep("md5.c"):sub(formatter, "FIle found:"):out("/tmp/luashelltest")
+ls(get_ls_args()):grep("md5.c"):sub(formatter, "FIle found:"):out("/tmp/luashelltest")()
 RED = cs(cat "/tmp/luashelltest")
 echo(RED)
 
